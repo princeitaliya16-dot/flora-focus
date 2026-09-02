6b991c7a8cd915ef438cf7c88f040b4ade434018
princeitaliya16-dot <princeitaliya16@gmail.com>
2026-09-02 18:22:15 +0530

    Restore initial OG web version: sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'florafocus-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './plants.js',
+  './particles.js',
+  './audio.js',
+  './timer.js',
+  './garden.js',
+  './economy.js',
+  './bees.js',
+  './apothecary.js',
+  './stats.js',
+  './app.js',
+  './icon.svg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).catch(() => {
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
